75a82df79cca91ea0f6390e40b16f65a26f07e75
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-09-22 19:34:24 +0530

    Scroll top and Zoom out Bug fix

---
+++ src/utils/ScrollToTopOnce.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+const ScrollBelowFixedNavbarOnce = () => {
+  useEffect(() => {
+    const firstVisit = sessionStorage.getItem("firstVisitDone");
+
+    if (!firstVisit) {
+      const navbar = document.querySelector("nav");
+      const navbarHeight = navbar ? navbar.offsetHeight : 80;
+      window.scrollTo({
+        top: navbarHeight + 40,
+        left: 0,
+        behavior: "smooth",
+      });
+
+      sessionStorage.setItem("firstVisitDone", "true");
+    }
+  }, []);
+
+  return null;
+};
+
+export default ScrollBelowFixedNavbarOnce;
